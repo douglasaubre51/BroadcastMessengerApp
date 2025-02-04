d8a156b5921d28d0e7c73cf9214c2cf45f9453a4
Allen Bose <boseallen192@gmail.com>
2025-02-04 13:26:44 +0530

    added create-account view as a tab and added required attribute to ProfilePhoto in create account View Model

--- BroadcastMvcApp/ViewModels/CreateAccountViewModel.cs
+++ BroadcastMvcApp/ViewModels/CreateAccountViewModel.cs
@@ -11,5 +11,6 @@ public class CreateAccountViewModel : Account
     [Compare("Password", ErrorMessage = "passwords do not match!")]
     public string? ConfirmPassword { get; set; }
 
+    [Required]
     public IFormFile? ProfilePhoto { get; set; }
 }
