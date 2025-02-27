1252875a0656dbe8eec8345bb11495908ae94c6d
Allen Bose <boseallen192@gmail.com>
2025-02-27 20:43:13 +0530

    1 togglable btns on cards in admin view

--- BroadcastMvcApp/wwwroot/js/admin/index.js
+++ BroadcastMvcApp/wwwroot/js/admin/index.js
@@ -1,35 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let flag = false
+    let addBtn = document.getElementsByClassName('add-btn')
 
-    while (flag == false) {
-        let addBtn = document.getElementsByClassName('add-btn')
+    for (let i = 0; i < addBtn.length; i++) {
+        addBtn[i].addEventListener('click', function () {
+            let addChannelList = document.getElementsByClassName('add-channel-list')
 
-        for (let i = 0; i < addBtn.length; i++) {
-            addBtn[i].addEventListener('click', function () {
-                let addChannelList = document.getElementsByClassName('add-channel-list')
-
-                if (!(addChannelList[i].textContent.trim() === "")) {
-                    flag = true
-                    addChannelList[i].classList.toggle('show')
-                    flag = false
-                }
-            })
-        }
+            if (!(addChannelList[i].textContent.trim() === "")) {
+                addChannelList[i].classList.toggle('show')
+            }
+        })
     }
 
-    while (flag == false) {
-        let removeBtn = document.getElementsByClassName('remove-btn')
+    let removeBtn = document.getElementsByClassName('remove-btn')
 
-        for (let i = 0; i < removeBtn.length; i++) {
-            removeBtn[i].addEventListener('click', function () {
-                let removeChannelList = document.getElementsByClassName('remove-channel-list')
+    for (let i = 0; i < removeBtn.length; i++) {
+        removeBtn[i].addEventListener('click', function () {
+            let removeChannelList = document.getElementsByClassName('remove-channel-list')
 
-                if (!(removeChannelList[i].textContent.trim() === "")) {
-                    flag = true
-                    removeChannelList[i].classList.toggle('show')
-                    flag = false
-                }
-            })
-        }
+            if (!(removeChannelList[i].textContent.trim() === "")) {
+                removeChannelList[i].classList.toggle('show')
+            }
+        })
     }
 })
